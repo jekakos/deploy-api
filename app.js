c347916003f39acb48883342fb694ce93d59059c
Kosivtsov Evgeniy <jekakos@gmail.com>
2023-10-31 14:37:00 +0400

    added branch check

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ app.get('/test-webhook', (req, res) => {
 });
 
 app.post('/github-webhook', (req, res) => {
-    exec('./deploy.sh', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-    });
+    const branch = req.body.ref.split('/').pop();
+    console.log(`Webhook with branch: ${branch}`);
+    
+    if (branch === 'main') {
+        exec('./deploy.sh', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+        });
+    }
     
     res.status(200).send('Received');
 });
