ca662f83cac094cce2844a65e64aaa15ef6c10cb
Kosivtsov Evgeniy <jekakos@gmail.com>
2023-11-02 22:54:33 +0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,13 @@ cd ~/ym-api
 git pull origin main
 
 cd ~/deploy
-docker-compose down
-docker-compose build --no-cache
+docker-compose down || exit 1
+sleep 5
+docker-compose build --no-cache || exit 1
 docker-compose up -d
+sleep 10
+docker builder prune -af
 docker image prune -af #delete unuse images
 docker container prune -f # Удаляет все остановленные контейнеры
 docker volume prune -f # Удаляет все неиспользуемые тома
 docker network prune -f # Удаляет все неиспользуемые сети
-docker builder prune -af
